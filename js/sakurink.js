84ead2166cc01adc0433b231381a0a8065a68643
Airhelym <74526568+Airhelym@users.noreply.github.com>
2024-01-22 22:55:07 +0800

    Site updated: 2024-01-22 22:55:05

---
+++ js/sakurink.js
@@ -0,0 +1,66 @@
+//部分功能js增强、自定义
+
+// https://github.com/MoePlayer/hexo-tag-aplayer/blob/master/docs/README-zh_cn.md#pjax-%E5%85%BC%E5%AE%B9
+// PJAX 兼容，手动清理 APlayer 全局实例
+/*$(document).on('pjax:start', function () {
+    if (window.aplayers) {
+        for (let i = 0; i < window.aplayers.length; i++) {
+            window.aplayers[i].destroy();
+        }
+        window.aplayers = [];
+    }
+});*/
+
+// Aplayer默认关闭歌词
+function removelrc() {
+  // 检测是否存在歌词按钮
+  const lrcIcon = document.querySelector(".aplayer-icon-lrc");
+  if (!lrcIcon) {
+    return;
+  }
+
+  // 触发以后立刻移除监听
+  observer.disconnect();
+
+  // 稍作延时保证触发函数时存在按钮
+  setTimeout(() => {
+    // 以触发按钮的方式隐藏歌词，防止在点击显示歌词按钮时需要点击两次才能出现的问题
+    lrcIcon.click();
+  }, 1);
+
+  console.log("success");
+}
+
+const observer = new MutationObserver((mutationsList, observer) => {
+  for (let mutation of mutationsList) {
+    if (mutation.type === "childList") {
+      removelrc();
+    }
+  }
+});
+
+const observerConfig = {
+  childList: true, // 观察子节点的变化
+  subtree: true, // 观察所有后代节点的变化
+};
+
+observer.observe(document, observerConfig); // 开始观察document节点的变化
+
+
+
+// 进入离开页面时修改标题
+var originTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        document.title = 'w(ﾟДﾟ)w不要走啊！！！ ' + originTitle;
+        if (titleTime != null) {
+            clearTimeout(titleTime);
+        }
+    } else {
+        document.title = 'ヾ(^▽^*)))欢迎回来！ ' + originTitle;
+        titleTime = setTimeout(function () {
+            document.title = originTitle;
+        }, 2000);
+    }
+});
