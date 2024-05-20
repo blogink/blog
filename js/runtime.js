85c3adada237b24946d2e4b9e65bb038d6221aa2
Airhelym <74526568+Airhelym@users.noreply.github.com>
2024-05-20 22:52:17 +0800

    Site updated: 2024-05-20 22:52:14

--- js/runtime.js
+++ js/runtime.js
@@ -1,5 +1,7 @@
 setInterval(() => {
-  let create_time = Math.round(new Date('2023-6-19 00:00:00').getTime() / 1000); //在此行修改建站时间
+  // let create_time = Math.round(new Date('2023-6-19 00:00:00').getTime() / 1000); //（原）在此行修改建站时间
+  // 有苹果用户发现safari浏览器不能正常运行，百度了一下发现是格式化的问题，改成下面这种应该就可以了。感谢反馈。
+  let create_time = Math.round(new Date('2023/6/19 00:00:00').getTime() / 1000); //在此行修改建站时间
   let timestamp = Math.round((new Date().getTime()) / 1000);
   let second = timestamp - create_time;
   let time = new Array(0, 0, 0, 0, 0);
